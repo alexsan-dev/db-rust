596b44dec4a5490200cd0263c364d050e3e1fdd8
Alex Santos <alexdsantosv@gmail.com>
2022-03-02 09:57:30 -0600

    [ Add ] pre launch tasks

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-CLASSPATH=.:/usr/local/lib/antlr-4.9-complete.jar
-alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.9-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.9-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
-cd lang
-antlr4 -Dlanguage=Go -o ../parser DBRust.g4 DBRustLexer.g4
-cd ..
+./build.sh
 go run main.go
